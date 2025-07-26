f5642a339ed8583651131aecd5ceff715d839555
levn201 <levn.barth07@gmail.com>
2025-07-27 00:22:20 +0200

    playground einladen

--- KahootTransnetBW/Pages/1Viewer/Playground.cshtml.cs
+++ KahootTransnetBW/Pages/1Viewer/Playground.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
 using KahootTransnetBW.Model;
+using Mysqlx.Crud;
 
 namespace KahootTransnetBW.Pages._1Viewer
 {
@@ -11,7 +12,7 @@ namespace KahootTransnetBW.Pages._1Viewer
         public List<FragenChecknerModel> FragenChecken { get; set; } = new();
 
 
-        [BindProperty(SupportsGet = true)] // <-- WICHTIG
+        [BindProperty(SupportsGet = true)] 
         public int GamePin { get; set; }
 
 
@@ -20,6 +21,26 @@ namespace KahootTransnetBW.Pages._1Viewer
         {
             LadeAlleFragen();
         }
+
+
+        // Berechtet wie viele Fragen der Fragebogen hat!
+        public int HowManyQuestions()
+        {
+            var db = new SQLconnection.DatenbankZugriff();
+            using var connection = db.GetConnection();
+            connection.Open();
+
+            string query = @"SELECT COUNT(*) FROM Fragen WHERE FragebogenID = @ID;";
+
+            using var cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@ID", GamePin);
+
+            // ExecuteScalar liefert das erste Feld der ersten Zeile des Ergebnisses
+            int countNumber = Convert.ToInt32(cmd.ExecuteScalar());
+
+            return countNumber;
+        }
+
 
 
         // Lade die Fragen 
@@ -31,33 +52,40 @@ namespace KahootTransnetBW.Pages._1Viewer
                 using var connection = db.GetConnection();
                 connection.Open();
 
-                string query = @"
-            SELECT 
-                Fragestellung,
-                Antwort1, IstAntwort1Richtig,
-                Antwort2, IstAntwort2Richtig,
-                Antwort3, IstAntwort3Richtig,
-                Antwort4, IstAntwort4Richtig
-            FROM Fragen 
-            WHERE FragebogenID = @ID; 
-        ";
+                string query = @" 
+                Select 
+                    Fragestellung,
+                    Antwort1, IstAntwort1Richtig,
+                    Antwort2, IstAntwort2Richtig,
+                    Antwort3, IstAntwort3Richtig,
+                    Antwort4, IstAntwort4Richtig
+                FROM Fragen
+                WHERE FragebogenID = @ID
+                ORDER BY FragebogenID
+                LIMIT 1 OFFSET 1;
+
+                ";
 
                 using var cmd = new MySqlCommand(query, connection);
-                cmd.Parameters.AddWithValue("@ID", GamePin); // <-- Parameter zuerst setzen!
+                cmd.Parameters.AddWithValue("@ID", GamePin); 
 
-                using var reader = cmd.ExecuteReader();      // <-- Dann ExecuteReader aufrufen
+                using var reader = cmd.ExecuteReader();     
 
                 while (reader.Read())
                 {
                     FragenChecken.Add(new FragenChecknerModel
                     {
                         DB_Fragestellung = reader.GetString("Fragestellung"),
+
                         DB_Antwort1 = reader.GetString("Antwort1"),
                         DB_IstAntwort1Richtig = reader.GetBoolean("IstAntwort1Richtig"),
+
                         DB_Antwort2 = reader.GetString("Antwort2"),
                         DB_IstAntwort2Richtig = reader.GetBoolean("IstAntwort2Richtig"),
+
                         DB_Antwort3 = reader.GetString("Antwort3"),
                         DB_IstAntwort3Richtig = reader.GetBoolean("IstAntwort3Richtig"),
+
                         DB_Antwort4 = reader.GetString("Antwort4"),
                         DB_IstAntwort4Richtig = reader.GetBoolean("IstAntwort4Richtig"),
                     });
@@ -68,6 +96,10 @@ namespace KahootTransnetBW.Pages._1Viewer
                 Console.WriteLine($"Fehler beim Laden: {ex.Message}");
             }
         }
+
+
+
+
 
 
         // Alle Propertys werden Aufgezeichnet 
