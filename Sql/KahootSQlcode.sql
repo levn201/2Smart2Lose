f27610c0fca19f5c4e67a64a417a337d2e72a254
Levin <levn.barth07@gmail.com>
2025-09-10 15:46:41 +0200

    Update KahootSQlcode.sql

--- Sql/KahootSQlcode.sql
+++ Sql/KahootSQlcode.sql
@@ -1,32 +1,53 @@
 Create Database KahootDatabase;
 use KahootDatabase; 
 
-Create Table test(
-	id int primary key auto_increment,
-    Name varchar(255),
-    ErstelltAm Datetime
+
+-- Drop table AdminUser; 							Tabelle Löschen
+-- Delete from fragebogen where Join_ID = 1212;		Inhalt löschen 
+                        
+create table dasboarduser (
+	ID_USER int primary key auto_increment, 
+    password varchar(255),
+    User varchar(255),
+    role varchar(255)
 );
 
-Create Table AdminUser(
-	User_ID int primary key auto_increment,
-    Username varchar(255),
-    password varchar(255)
+Create Table PlayerPoints(
+	ID_Session int primary key auto_increment,
+    User_Nickname varchar(255),
+    SessionPints int, 
+    GamePin int ,
+    CorrectAnswered int, 
+    PossibleAnswers int, 
+    saveTime DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+
+
+CREATE TABLE Fragen (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FragebogenID INT NOT NULL,
+    Fragestellung TEXT NOT NULL,
+    Antwort1 TEXT NOT NULL,
+    IstAntwort1Richtig TINYINT(1),
+    Antwort2 TEXT NOT NULL,
+    IstAntwort2Richtig TINYINT(1),
+    Antwort3 TEXT NOT NULL,
+    IstAntwort3Richtig TINYINT(1),
+    Antwort4 TEXT NOT NULL,
+    IstAntwort4Richtig TINYINT(1),
+    CONSTRAINT chk_mindestens_eine_richtig
+        CHECK (
+            IstAntwort1Richtig = 1
+            OR IstAntwort2Richtig = 1
+            OR IstAntwort3Richtig = 1
+            OR IstAntwort4Richtig = 1
+        )
 );
 
-INSERT INTO AdminUser (Username, Password)
-VALUES 
-('admin', 123),
-('levin', 123);
 
-
-Create Table CreaterUser(
-	Creater_ID int primary key auto_increment,
-    Username varchar(255),
-    password varchar(255)
+CREATE TABLE Fragebogen (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+	Join_ID INT,
+    Titel TEXT NOT NULL,
+    ErstelltAm DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO CreaterUser (Username, Password)
-VALUES 
-('User', 222);
-
--- Drop table AdminUser;
