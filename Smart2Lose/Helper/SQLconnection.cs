89633f6cdaa309e1540b54dda8ea56e532a1c852
levn201 <levn.barth07@gmail.com>
2026-01-04 02:11:09 +0100

    Connection 2 .0

--- Smart2Lose/Helper/SQLconnection.cs
+++ Smart2Lose/Helper/SQLconnection.cs
@@ -11,7 +11,7 @@ namespace Smart2Lose.Helper
             public string connectionString =
 
             /*"Server=localhost;Database=KahootDatabase;Uid=root;Pwd=21481TNGhello!";*/  //Connection zur Framework Local Database von Levin 
-            "Server=192.168.200.30;Port=3306;Database=KahootDatabase;Uid=root;Pwd=TNBWazubi1!";
+            "Server=192.168.200.30;Port=3306;Database=KahootDatabase;Uid=kahootUser;Pwd=TNBWazubi1!;SslMode=None;";
 
             public MySqlConnection GetConnection()
             {
